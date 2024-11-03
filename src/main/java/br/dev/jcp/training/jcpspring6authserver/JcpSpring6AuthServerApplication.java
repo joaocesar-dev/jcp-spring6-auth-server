4628a7eeafb4ff48012327181ac3ac82ce16e9da
Joao Cesar <joao.cesar@jcp.dev.br>
2024-11-03 19:12:05 -0300

    Initial Commit

---
+++ src/main/java/br/dev/jcp/training/jcpspring6authserver/JcpSpring6AuthServerApplication.java
@@ -0,0 +1,13 @@
+package br.dev.jcp.training.jcpspring6authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JcpSpring6AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JcpSpring6AuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/jcp/training/jcpspring6authserver/JcpSpring6AuthServerApplicationTests.java
@@ -0,0 +1,14 @@
+package br.dev.jcp.training.jcpspring6authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JcpSpring6AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Just test the load App
+    }
+
+}
